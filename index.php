455545f11dcd7f2160287ef7de1755adc3ae9fce
simonebonifazi <simonbonifraz@gmail.com>
2022-08-29 18:11:55 +0200

    added-paragraph

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>First PHP doc</title>
-</head>
-<body>
-    <p>
-        ciao a tutti
-    </p>
-</body>
-</html>
+<?php
+
+
+
+?>
+
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore voluptatum adipisci in ratione, debitis quis officia a. A necessitatibus illum voluptatem, vitae, velit amet non, repellat ullam sint debitis quis.</p>
