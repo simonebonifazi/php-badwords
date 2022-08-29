674c03517f3aefeb0d5cca27c17d7f032098ee4a
simonebonifazi <simonbonifraz@gmail.com>
2022-08-29 18:18:32 +0200

    added badword

--- index.php
+++ index.php
@@ -3,5 +3,16 @@
 
 
 ?>
-
-<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore voluptatum adipisci in ratione, debitis quis officia a. A necessitatibus illum voluptatem, vitae, velit amet non, repellat ullam sint debitis quis.</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My PHP first doc</title>
+</head>
+<body>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit  puellae. Inventore voluptatum adipisci in ratione, debitis quis officia a puellae. A necessitatibus illum voluptatem, vitae puellae, velit amet non, repellat ullam sint debitis quis puellae.</p>
+    
+</body>
+</html>
